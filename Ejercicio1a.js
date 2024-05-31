40af333e0a6ac1beb6d8640a59511f8caf12f1df
Sara Jaramillo <sarasunrise45@gmail.com>
2024-05-31 12:08:37 -0500

    Primer ejercicio

---
+++ Ejercicio1a.js
@@ -0,0 +1,11 @@
+let sueldo = 1800;
+let antiguedad = 12;
+
+console.log("i. ¿El sueldo es inferior a $2000 US y su antiguedad es igual o superior a 10 años?");
+console.log(sueldo < 2000 && antiguedad >=10);
+
+console.log("ii. ¿El sueldo es inferior a $2000 US o su antiguedad es menor a 10 años?");
+console.log(sueldo < 2000 || antiguedad <10);
+
+console.log("iii. ¿El sueldo es mayor o igual a $2000?");
+console.log(sueldo >=  2000)
