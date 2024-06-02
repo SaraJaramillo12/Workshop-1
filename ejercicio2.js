9303119386cba861014de0393bb355172d4bbc4a
Carolina David <carolinadavidsucerquia97@gmail.com>
2024-06-02 16:08:13 -0500

    Ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,39 @@
+//. Ejercicio 2.
+// Declaracion de variables
+
+const a = 4;
+const b = 7;
+const c = 2 * b;
+const x = a;
+const y = 3 * x;
+const u = 6;
+const w = 10;
+const z = 2 * w;
+
+//Ejercicio 2a
+
+resultado = a/ (b * c);
+console.log("2a. El resultado de la operacion a / bc es: ",resultado);
+
+//Eejercicio 2b
+
+const a_cuadrado = Math.pow(a, 2);
+const b_al_92 = Math.pow(b, 92);
+
+const resultado2 = a_cuadrado + b_al_92;
+
+console.log("2b. El resultado de la expresión a^2 + b^92 es:", resultado2);
+
+
+//Ejercicio 2c
+
+resultado = (x + y) / (u + w / a);
+console.log("2c. El resultado de la operacion es: ", resultado);
+
+//Ejercicio 2d
+
+resultado3 = x / y * (z + w);
+console.log("2d. El resultado de la operación x / y * (z + w) es ", resultado3);
+
+
+
